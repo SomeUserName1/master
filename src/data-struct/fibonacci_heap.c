eadffa81d0db3b282f7994696f6d01872956933c
Fabian Klopfer <fabian.klopfer@ieee.org>
2021-03-18 22:38:34 +0100

    G230 door locked me out after taking a peeing break... ffs

--- src/data-struct/fibonacci_heap.c
+++ src/data-struct/fibonacci_heap.c
@@ -61,5 +61,86 @@ destroy_fib_heap(fib_heap_t* fh)
 }
 
 int
-fib_heap_insert(fib_heap_t* fh)
-{}
+fib_heap_insert(fib_heap_t* fh, fib_node* node)
+{
+    if (!fh || !node) {
+        exit(-1);
+    }
+
+    if (!fh->min) {
+        fh->min = node;
+        node->left = node;
+        node->right = node;
+    } else {
+        node->right = fh->min->right;
+        node->right->left = node;
+        node->left = fh->min;
+        fh->min->right = node;
+
+        if (fh->min->key > node->key) {
+            fh->min = node;
+        }
+    }
+    fh->num_nodes++;
+
+    return 0;
+}
+
+void*
+fib_heap_min(fib_heap_t* fh)
+{
+    if (!fh) {
+        exit(-1);
+    }
+    return fh->min;
+}
+
+void*
+fib_heap_extract_min(fib_heap_t* fh) {
+    if (!fh) {
+        exit(-1);
+    }
+
+    fib_node* z = fh->min;
+
+    if (z) {
+        // FIXME continue here with p.513 intro to algos, corman
+    }
+}
+
+fib_heap_t*
+fib_heap_union(fib_heap_t* fh1, fib_heap_t* fh2)
+{
+    if (!fh1 || !fh2) {
+        exit(-1);
+    }
+
+    fib_heap_t* fh = create_fib_heap();
+
+    if (fh1->num_nodes == 0 && fh2->num_nodes == 0) {
+        return fh;
+    }
+    if (fh1->num_nodes == 0 || fh2->num_nodes == 0) {
+        fh->min = fh1->num_nodes == 0 ? fh2->min : fh1->min;
+    } else if (fh1->num_nodes > 0 && fh2->num_nodes > 0) {
+        fib_node* fh2_temp;
+        fib_node* fh1_temp;
+
+        fh->min = fh1->min;
+
+        fh1_temp = fh1->min->right;
+        fh2_temp = fh2->min->left;
+        fh1->min->right = fh2->min;
+        fh2->min->left = fh1->min;
+        fh1_temp->left = fh2_temp;
+        fh2_temp->right = fh1_temp;
+
+        if (fh2->min->key < fh->min->key) {
+            fh->min = fh2->min;
+        }
+    }
+
+    fh->num_nodes = fh1->num_nodes + fh2->num_nodes;
+
+    return fh;
+}
