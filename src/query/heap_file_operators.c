80507aede8520c40addba2c36113aaf6f7e9e2eb
Fabian Klopfer <fabian.klopfer@ieee.org>
2021-07-27 11:40:14 +0200

    safe

---
+++ src/query/heap_file_operators.c
@@ -0,0 +1,30 @@
+
+#include "access/heap_file.h"
+#include "access/relationship.h"
+
+array_list_node*
+get_nodes(heap_file* hf)
+{}
+
+array_list_relationship*
+get_relationships(heap_file* hf)
+{}
+
+unsigned long
+next_relationship_id(heap_file*      hf,
+                     unsigned long   node_id,
+                     relationship_t* rel,
+                     direction_t     direction)
+{}
+
+array_list_relationship*
+expand(heap_file* db, unsigned long node_id, direction_t direction)
+{}
+
+relationship_t*
+contains_relationship_from_to(heap_file*    hf,
+                              unsigned long node_from,
+                              unsigned long node_to,
+                              direction_t   direction)
+{}
+
