4ab44a85031b6d5f787dbe5b98be5250ba62813d
Fabian Klopfer <fabian.klopfer@ieee.org>
2021-08-17 18:30:58 +0200

    page cache test create, destroy

--- test/cache/page_cache_test.c
+++ test/cache/page_cache_test.c
@@ -1,9 +1,13 @@
+#include "data-struct/bitmap.h"
+#include "data-struct/htable.h"
 #include "page_cache.h"
 
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "constants.h"
+#include "data-struct/linked_list.h"
 #include "physical_database.h"
 
 void
@@ -29,13 +33,56 @@ test_page_cache_create(void)
 #endif
     );
     assert(pc);
+    assert(pc->pdb == pdb);
+    assert(pc->total_pinned == 0);
+    assert(pc->total_unpinned == 0);
+    assert(llist_ul_size(pc->free_frames) == CACHE_N_PAGES);
+    for (size_t i = 0; i < CACHE_N_PAGES; ++i) {
+        assert(get_bit(pc->pinned, i) == 0);
+    }
+    assert(queue_ul_size(pc->recently_referenced) == 0);
+
+    for (size_t i = 0; i < invalid; ++i) {
+        assert(dict_ul_ul_size(pc->page_map[i]) == 0);
+        dict_ul_ul_destroy(pc->page_map[i]);
+    }
+
     assert(pc->cache);
-    // TODO continue here
+    bitmap_destroy(pc->pinned);
+    llist_ul_destroy(pc->free_frames);
+    queue_ul_destroy(pc->recently_referenced);
+    free(pc);
+    phy_database_delete(pdb);
 }
 
 void
 test_page_cache_destroy(void)
-{}
+{
+    char* file_name = "test";
+
+#ifdef VERBOSE
+    char* log_name_pdb   = "log_test_pdb";
+    char* log_name_cache = "log_test_cache";
+#endif
+
+    phy_database* pdb = phy_database_create(file_name
+#ifdef VERBOSE
+                                            ,
+                                            log_name_pdb
+#endif
+    );
+    page_cache* pc = page_cache_create(pdb
+#ifdef VERBOSE
+                                       ,
+                                       log_name_cache
+#endif
+    );
+
+    page_cache_destroy(pc);
+
+    assert(pdb);
+    phy_database_delete(pdb);
+}
 
 void
 test_new_page(void)
